50810f75e7e7516c3460c7608a4ded670f4b6181
Shota <149574372+TaroYamada1997@users.noreply.github.com>
2025-03-08 17:39:30 +0900

    fix: メタデータを更新

--- constants/metadata.ts
+++ constants/metadata.ts
@@ -13,5 +13,20 @@ export const metadata: Metadata = {
     title: "スプラトゥーン クイズ | Splatoon Quiz Game",
     description: "スプラトゥーンに関する知識を試せる無料クイズゲーム。",
     type: "website",
+    url: "https://splatoon3-quiz.com",
+    images: [
+      {
+        url: "icon/icon_black.svg", // OGP画像のパス
+        width: 1200,
+        height: 630,
+        alt: "スプラトゥーン クイズ",
+      },
+    ],
+    siteName: "スプラトゥーン クイズ",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@YourTwitterHandle", // あなたのTwitterハンドル（任意）
+    images: ["https://splatoon3-quiz.com/ogp.png"],
   },
 };
