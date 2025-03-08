50ddca99bb0f16e0cc0f68cddd3dd4f9b5b6ffe1
Shota <149574372+TaroYamada1997@users.noreply.github.com>
2025-03-08 17:28:14 +0900

    fix: メタデータ取得処理を修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,11 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import { metadata as siteMetadata } from "@/constants/metadata";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = siteMetadata;
 
 export default function RootLayout({
   children,
@@ -12,26 +15,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <head>
-        <title>スプラトゥーン クイズ | Splatoon Quiz Game</title>
-        <meta
-          name="description"
-          content="スプラトゥーンに関する知識を試せる無料クイズゲーム。様々なクイズに挑戦しよう！ スプラトゥーン3対応"
-        />
-        <meta
-          name="keywords"
-          content="スプラトゥーン,スプラトゥーン3,クイズ,ゲーム,Splatoon,Quiz,無料,ブキ,ギア"
-        />
-        <meta
-          property="og:title"
-          content="スプラトゥーン クイズ | Splatoon Quiz Game"
-        />
-        <meta
-          property="og:description"
-          content="スプラトゥーンに関する知識を試せる無料クイズゲーム。"
-        />
-        <meta property="og:type" content="website" />
-      </head>
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
           <Header />
