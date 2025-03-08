98e73281a66bf9b8afa80af683ff103ae4cc9c1f
Shota <149574372+TaroYamada1997@users.noreply.github.com>
2025-03-08 17:27:39 +0900

    fix: メタデータにファビコン等を追加

--- constants/metadata.ts
+++ constants/metadata.ts
@@ -1,6 +1,17 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "スプラトゥーン3 ブキクイズ",
-  description: "サブとスペシャルからメインブキを当てるクイズゲーム",
+  title: "スプラトゥーン クイズ | Splatoon Quiz Game",
+  description:
+    "スプラトゥーンに関する知識を試せる無料クイズゲーム。ブキ、ギア、ステージなど様々なクイズに挑戦しよう！ スプラトゥーン3対応",
+  keywords:
+    "スプラトゥーン,スプラトゥーン3,クイズ,ゲーム,Splatoon,Quiz,無料,ブキ,ギア",
+  icons: {
+    icon: "/icon/favicon/favicon.ico",
+  },
+  openGraph: {
+    title: "スプラトゥーン クイズ | Splatoon Quiz Game",
+    description: "スプラトゥーンに関する知識を試せる無料クイズゲーム。",
+    type: "website",
+  },
 };
